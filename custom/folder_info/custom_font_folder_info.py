27bcd99317748eb6a3596d1e1137bfd50244b270
SIH-D-2642\Administrator <hmq>
2024-10-17 20:22:59 +0800

    储存

---
+++ custom/folder_info/custom_font_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {134235886: (2147483647, 0), 134252533: (2147483647, 1)}}
